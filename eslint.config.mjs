13dba68a925898bd1795edfca638230aa7a095ab
codingwithvm <victormmendes.dev@gmail.com>
2026-04-19 18:53:23 -0300

    chore: add node globals for `infra/scripts` in `eslint.config.mjs` and update `.prettierignore`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import { defineConfig } from "eslint/config";
 import tseslint from "typescript-eslint";
 import prettierConfig from "eslint-config-prettier";
+import globals from "globals";
 
 export default defineConfig([
   {
@@ -11,9 +12,16 @@ export default defineConfig([
       "**/dist/**",
       "**/build/**",
       "**/next-env.d.ts",
+      "**/src/generated/**",
     ],
   },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   prettierConfig,
+  {
+    files: ["scripts/**/*.mjs", "apps/*/infra/scripts/**/*.mjs"],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
 ]);
